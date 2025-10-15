67a626e90d0d166cd7117ac6a7b5ad3f3d4d8ab0
pezzaliapp <pezzalialessandro@gmail.com>
2025-10-15 22:19:11 +0200

    Update main function and add video initialization
    Refactor main function to initialize video and draw a test pattern each frame.

--- doomgeneric/doomgeneric.c
+++ doomgeneric/doomgeneric.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
-#include <emscripten.h>
+#include <emscripten/emscripten.h>
 
-EMSCRIPTEN_KEEPALIVE
-int main() {
-    printf("DoomGenericWASM engine started.\\n");
-    emscripten_set_main_loop([](){}, 0, 1);
-    return 0;
+// dichiarazioni implementate in doomgeneric_sdl.c
+int DG_InitVideo(void);
+void DG_DrawTestPattern(void);
+
+// piccolo main di test: inizializza e disegna qualcosa ogni frame
+static void main_loop(void) {
+  DG_DrawTestPattern();
+}
+
+int main(void) {
+  printf("[DoomGenericWASM] start\n");
+
+  if (!DG_InitVideo()) {
+    printf("[DoomGenericWASM] SDL init failed\n");
+    return 1;
+  }
+
+  // 0 fps => il browser decide; 1 => loop principale gestito da emscripten
+  emscripten_set_main_loop(main_loop, 0, 1);
+  return 0;
 }
